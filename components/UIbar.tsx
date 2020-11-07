48af2a0ec45d38ebb2da562a217b4ae2fab3ccf3
deno003 <52130732+deno003@users.noreply.github.com>
2020-11-08 01:26:47 +0900

    UIbar追加

---
+++ components/UIbar.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import clsx from 'clsx'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import IconButton from '@material-ui/core/IconButton'
+import Badge from '@material-ui/core/Badge'
+import MenuIcon from '@material-ui/icons/Menu'
+import NotificationsIcon from '@material-ui/icons/Notifications'
+import styles from '~/styles/Home.module.css'
+
+const UIbar = () => {
+  const [open, setOpen] = React.useState(true)
+  const handleDrawerOpen = () => {
+    setOpen(true)
+  }
+  const handleDrawerClose = () => {
+    setOpen(false)
+  }
+
+  return (
+    <AppBar
+      position="absolute"
+      className={clsx(styles.appBar, open && styles.appBarShift)}
+    >
+      <Toolbar className={styles.toolbar}>
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="open drawer"
+          onClick={handleDrawerOpen}
+          className={clsx(styles.menuButton, open && styles.menuButtonHidden)}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography
+          component="h1"
+          variant="h6"
+          color="inherit"
+          noWrap
+          className={styles.title}
+        >
+          Dashboard
+        </Typography>
+        <IconButton color="inherit">
+          <Badge badgeContent={4} color="secondary">
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  )
+}
+
+export default UIbar
